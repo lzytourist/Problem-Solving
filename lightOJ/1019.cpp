47d64db16e8b5414a00b5a95b87a72703bd930a1
Monowar Hossain Khan <48186359+lzytourist@users.noreply.github.com>
2021-01-27 19:44:34 +0600

    Update 1019.cpp

--- lightOJ/1019.cpp
+++ lightOJ/1019.cpp
@@ -63,16 +63,16 @@ int main()
 	// ios_base::sync_with_stdio(false);
 	// cin.tie(nullptr);
 	// cout.tie(nullptr);
-	
+
 	int t;
 	scanf("%d", &t);
 	// cin >> t;
-	
+
 	for(int i = 1; i <= t; ++i) {
 		printf("Case %d: ", i);
 		// cout << "Case " << i << ": ";
 		doomed();
 	}
-	
+
 	return 0;
 }

2ebaa778df0ac52d1dec148ae8b571904b49203a
Monowar Hossain Khan <mhkha2022@gmail.com>
2022-09-05 22:05:21 +0600

    Light OJ - Goldbach's Conjecture
    https://lightoj.com/problem/goldbach-s-conjecture

--- lightOJ/1259.cpp
+++ lightOJ/1259.cpp
@@ -14,49 +14,52 @@ const int N = 1e7;
 bitset<N> prime;
 
 void gen() {
-	for (int i = 3; 1LL * i * i <= 1LL * N; i += 2) {
-		if (!prime[i]) {
-			for (ll j = 1LL * i * i; j < N; j += 2LL * i) {
-				prime[j] = true;
-			}
-		}
-	}
+    for (int i = 3; 1LL * i * i <= 1LL * N; i += 2) {
+        if (!prime[i]) {
+            for (ll j = 1LL * i * i; j < N; j += 2LL * i) {
+                prime[j] = true;
+            }
+        }
+    }
 }
 
 inline bool isPrime(int n) {
-	return (n & 1) && prime[n] != true;
+    if (n == 2) {
+        return true;
+    }
+    return (n & 1) && prime[n] != true;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	#ifdef LZYTOURIST
-	freopen("/home/joker/Documents/code/in.txt", "r", stdin);
-	freopen("/home/joker/Documents/code/out.txt", "w", stdout);
-	#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    #ifdef LZYTOURIST
+    freopen("/home/joker/Documents/code/in.txt", "r", stdin);
+    freopen("/home/joker/Documents/code/out.txt", "w", stdout);
+    #endif
 
-	gen();
+    gen();
 
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	for (int tc = 1; tc <= t; ++tc) {
-		cout << "Case " << tc << ": ";
+    for (int tc = 1; tc <= t; ++tc) {
+        cout << "Case " << tc << ": ";
 
-		int n;
-		cin >> n;
-		
-		int cnt = 0;
-		for (int i = 2; i <= n - i; ++i) {
-			if (isPrime(i) && isPrime(n - i)) {
-				++cnt;
-			}
-		}
+        int n;
+        cin >> n;
+        
+        int cnt = 0;
+        for (int i = 2; i <= n - i; ++i) {
+            if (isPrime(i) && isPrime(n - i)) {
+                ++cnt;
+            }
+        }
 
-		cout << cnt << endl;
-	}
+        cout << cnt << endl;
+    }
 
-	return 0;
+    return 0;
 }

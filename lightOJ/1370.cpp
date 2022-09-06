109541ccd51eb3c1f7df546c384e487e8a446399
Monowar Hossain Khan <mhkha2022@gmail.com>
2022-09-06 16:00:02 +0600

    Light OJ - Bi-shoe and Phi-shoe
    https://lightoj.com/problem/bi-shoe-and-phi-shoe

--- lightOJ/1370.cpp
+++ lightOJ/1370.cpp
@@ -14,64 +14,64 @@ const int N = 2e6;
 vector<int> primes;
 
 void gen() {
-	bitset<N> prime;
+    bitset<N> prime;
 
-	for (int i = 3; 1LL * i * i <= N; i += 2) {
-		for (ll j = 1LL * i * i; j < N; j += 2LL * i) {
-			prime[j] = true;
-		}
-	}
+    for (int i = 3; 1LL * i * i <= N; i += 2) {
+        for (ll j = 1LL * i * i; j < N; j += 2LL * i) {
+            prime[j] = true;
+        }
+    }
 
-	primes.pb(2);
-	for (int i = 3; i < N; i += 2) {
-		if (!prime[i]) {
-			primes.pb(i);
-		}
-	}
+    primes.pb(2);
+    for (int i = 3; i < N; i += 2) {
+        if (!prime[i]) {
+            primes.pb(i);
+        }
+    }
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	#ifdef LZYTOURIST
-	freopen("/home/joker/Documents/code/in.txt", "r", stdin);
-	freopen("/home/joker/Documents/code/out.txt", "w", stdout);
-	#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    #ifdef LZYTOURIST
+    freopen("/home/joker/Documents/code/in.txt", "r", stdin);
+    freopen("/home/joker/Documents/code/out.txt", "w", stdout);
+    #endif
 
-	gen();
+    gen();
 
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
 
-	for (int tc = 1; tc <= t; ++tc) {
-		cout << "Case " << tc << ": ";
+    for (int tc = 1; tc <= t; ++tc) {
+        cout << "Case " << tc << ": ";
 
-		int n;
-		cin >> n;
+        int n;
+        cin >> n;
 
-		ll cost = 0LL;
-		for (int i = 0, x; i < n; ++i) {
-			cin >> x;
+        ll cost = 0LL;
+        for (int i = 0, x; i < n; ++i) {
+            cin >> x;
 
-			int l = 0, r = primes.size() - 1, m, p = 0;
-			while (l <= r) {
-				m = l + (r - l) / 2;
+            int l = 0, r = primes.size() - 1, m, p = 0;
+            while (l <= r) {
+                m = l + (r - l) / 2;
 
-				if (primes[m] > x) {
-					p = m;
-					r = m - 1;
-				} else {
-					l = m + 1;
-				}
-			}
+                if (primes[m] > x) {
+                    p = m;
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
 
-			cost += 1LL * primes[p];
-		}
+            cost += 1LL * primes[p];
+        }
 
-		cout << cost << " Xukha" << endl;
-	}
+        cout << cost << " Xukha" << endl;
+    }
 
-	return 0;
+    return 0;
 }
